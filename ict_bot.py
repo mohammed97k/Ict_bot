ae2aebe0a9a6ca15bc4c1a8c9df0a7393ea48c9c
mohammed97k <mhmdkhaldallwyzy@gmail.com>
2026-09-12 03:48:53 +0300

    Update ict_bot.py

--- ict_bot.py
+++ ict_bot.py
@@ -373,7 +373,6 @@ def main():
             process_symbol(name, cfg, state, now_utc)
         except Exception as e:
             print(f"[{name}] خطأ غير متوقع: {e}")
-    send_telegram("✅ تم تشغيل البوت بنجاح والاتصال بتليجرام يعمل بشكل سليم!")
 
     save_state(state)
 
